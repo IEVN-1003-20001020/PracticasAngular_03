680ebf499db85a3f93e4619dc7d8b14153f41870
Milton Ramirez <ramitezmilton@gmail.com>
2023-09-29 21:17:29 -0600

    29/09/29 MAVR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { OperasBasComponent } from './calculos/operas-bas/operas-bas.component';
+import { PotenciasComponent } from './potencia/potencias/potencias.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CinepolisComponent } from './cinepolis/cinepolis/cinepolis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OperasBasComponent
+    OperasBasComponent,
+    PotenciasComponent,
+    CinepolisComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
